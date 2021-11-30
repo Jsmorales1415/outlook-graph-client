211e3dcfa2725e2b9c5b5fd832dabac7092bd978
Jsmorales1415 <jsmorales1415@gmail.com>
2021-11-30 10:03:00 -0500

    v2

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Graph Client v1</h1>
+      <h1>Graph Client v2</h1>
     </div>
   )
 }
